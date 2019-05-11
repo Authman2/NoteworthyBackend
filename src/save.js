40e6086b0de548d8f8eaeb14316540f763453371
Adeola Uthman <authman2@gmail.com>
2019-05-10 23:13:59 -0400

    new api version

--- src/save.js
+++ src/save.js
@@ -9,7 +9,9 @@ const handleSave = (server) => {
             // Get the current user.
             const params = typeof req.query === 'string' ? JSON.parse(req.query) : req.query;
             const uid = params.uid;
-            if(!uid) return rep.response('No user is currently logged in, so a note could not be created.').code(400);
+            if(!uid) {
+                return rep.response('No user is currently logged in, so a note could not be created.').code(400);
+            }
 
             // Get the list of notebooks and notes all together.
             const data = typeof req.payload === 'string' ? JSON.parse(req.payload) : req.payload;
@@ -19,8 +21,13 @@ const handleSave = (server) => {
             try {
                 const old = (await firebase.database().ref().child(uid).child(noteID).once('value')).val();
                 const saved = { ...old, title, content };
-                const result = await firebase.database().ref().child(uid).child(noteID).set(saved);
-                return rep.response(result).code(200);
+                
+                try {
+                    await firebase.database().ref().child(uid).child(noteID).set(saved);
+                    return rep.response('Saved!').code(200);
+                } catch(err) {
+                    return rep.response(''+err).code(500);
+                }
             } catch(err) {
                 return rep.response(''+err).code(500);
             }

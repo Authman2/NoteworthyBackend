e8a59418f87d3b9c5a32929ee1d8d3d72064e3e9
Adeola Uthman <authman2@gmail.com>
2019-05-11 23:53:58 -0400

    new api version

--- src/saveNotebook.js
+++ src/saveNotebook.js
@@ -1,29 +1,29 @@
 const firebase = require('firebase');
 
 // Saves the most recent version of the current note to the database.
-const handleSave = (server) => {
+const handleSaveNotebook = (server) => {
     server.route({
         method: 'post',
-        path: '/save',
+        path: '/save-notebook',
         async handler(req, rep) {
             // Get the current user.
             const params = typeof req.query === 'string' ? JSON.parse(req.query) : req.query;
             const uid = params['uid'];
             if(!uid) {
-                return rep.response('No user is currently logged in, so the note could not be saved.').code(400);
+                return rep.response('No user is currently logged in, so the notebook could not be saved.').code(400);
             }
 
             // Get the list of notebooks and notes all together.
             const data = typeof req.payload === 'string' ? JSON.parse(req.payload) : req.payload;
-            const { noteID, title, content } = data;
+            const { notebookID, pages } = data;
 
             // Save the entire structure to the database.
             try {
-                const old = (await firebase.database().ref().child(uid).child(noteID).once('value')).val();
-                const saved = { ...old, title, content };
+                const old = (await firebase.database().ref().child(uid).child(notebookID).once('value')).val();
+                const saved = { ...old, id: notebookID, pages: pages };
                 
                 try {
-                    await firebase.database().ref().child(uid).child(noteID).set(saved);
+                    await firebase.database().ref().child(uid).child(notebookID).set(saved);
                     return rep.response('Saved!').code(200);
                 } catch(err) {
                     return rep.response(''+err).code(500);
@@ -35,4 +35,4 @@ const handleSave = (server) => {
     });
 }
 
-module.exports = handleSave;
+module.exports = handleSaveNotebook;
